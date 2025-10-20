df5c93afbfc5aeb9a5a0c769d4e5b11a5bb6f429
Bernat <girobernat@gmail.com>
2025-10-20 18:20:36 +0200

    Model working

--- src/model.py
+++ src/model.py
@@ -2,13 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Helper class for positional embeddings for the timestep
 class SinusoidalPositionEmbeddings(nn.Module):
     def __init__(self, dim):
         super().__init__()
         self.dim = dim
 
-    def forward(self, time):
+    def forward(self, time): # Construct the embedding of the current timestep.
         device = time.device
         half_dim = self.dim // 2
         embeddings = torch.log(torch.tensor(10000.0, device=device)) / (half_dim - 1)
@@ -17,13 +16,13 @@ class SinusoidalPositionEmbeddings(nn.Module):
         embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
         return embeddings
 
-# A simple building block for our U-Net
+# Building block for the U-Net
 class Block(nn.Module):
     def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
         super().__init__()
         self.time_mlp = nn.Linear(time_emb_dim, out_ch)
         if up:
-            self.conv1 = nn.Conv2d(2 * in_ch, out_ch, 3, padding=1)
+            self.conv1 = nn.Conv2d(2 * in_ch, out_ch, 3, padding=1) # Expected to be concatenated with skip conn (2*in).
             self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1)
         else:
             self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
@@ -36,16 +35,16 @@ class Block(nn.Module):
     def forward(self, x, t):
         h = self.bnorm1(self.relu(self.conv1(x)))
         time_emb = self.relu(self.time_mlp(t))
-        time_emb = time_emb[(...,) + (None,) * 2]
+        time_emb = time_emb[(...,) + (None,) * 2] # (BatchSize,out_ch) to (BatchSize,out_ch,1,1)
         h = h + time_emb
         h = self.bnorm2(self.relu(self.conv2(h)))
         return self.transform(h)
 
-# The main U-Net model
+# U-Net model
 class SimpleUnet(nn.Module):
     def __init__(self, num_classes=10, class_emb_dim=4):
         super().__init__()
-        
+
         # Image settings
         image_channels = 1 # MNIST is grayscale
         down_channels = (64, 128, 256, 512, 1024)
@@ -59,34 +58,37 @@ class SimpleUnet(nn.Module):
             nn.Linear(time_emb_dim, time_emb_dim),
             nn.ReLU()
         )
-        
+
         # Class/Label embedding
         self.class_emb = nn.Embedding(num_classes, class_emb_dim)
+        # Project class embedding to time embedding dimension
+        self.class_mlp = nn.Linear(class_emb_dim, time_emb_dim)
 
         # Initial projection
         self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1)
 
         # U-Net Downsampling path
         self.downs = nn.ModuleList([Block(down_channels[i], down_channels[i+1], time_emb_dim) for i in range(len(down_channels)-1)])
-        
+
         # U-Net Upsampling path
         self.ups = nn.ModuleList([Block(up_channels[i], up_channels[i+1], time_emb_dim, up=True) for i in range(len(up_channels)-1)])
-        
+
         # The output layer
         self.output = nn.Conv2d(up_channels[-1], out_dim, 1)
 
     def forward(self, x, timestep, y):
         # Embed time and labels
         t = self.time_mlp(timestep)
-        
+
         # Simple conditional guidance: we add the class embedding to the time embedding
-        # This injects the label information into the model
+        # Project the class embedding to the time embedding dimension before adding
         class_emb = self.class_emb(y)
+        class_emb = self.class_mlp(class_emb)
         t = t + class_emb
 
         # Initial convolution
         x = self.conv0(x)
-        
+
         # U-Net
         residual_inputs = []
         for down in self.downs:
@@ -96,5 +98,5 @@ class SimpleUnet(nn.Module):
             residual_x = residual_inputs.pop()
             x = torch.cat((x, residual_x), dim=1) # Skip connection
             x = up(x, t)
-            
+
         return self.output(x)
